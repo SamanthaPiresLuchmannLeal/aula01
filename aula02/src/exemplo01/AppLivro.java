2b405d11e9ece1e0001785e436ca708663774fa4
SamanthaPiresLuchmannLeal <samantha.luchmann@mercadolivre.com>
2022-06-24 10:25:29 -0300

    Exemple 01

---
+++ aula02/src/exemplo01/AppLivro.java
@@ -0,0 +1,21 @@
+package exemplo01;
+
+public class AppLivro {
+    public static void main(String[] args) {
+        Livro livro1;
+        livro1 = new Livro();
+
+        Livro livro2 = new Livro();
+
+        livro1.nome = "Programando em Java";
+        livro1.autor = "Deitel";
+
+        livro2.nome = "Banco de Dados SQL";
+        livro2.autor = "Marcos da Silva";
+
+        System.out.println(livro1.nome);
+        System.out.println(livro1.autor);
+        System.out.println(livro2.nome);
+        System.out.println(livro2.autor);
+    }
+}
