393c8c31d665557929c4b3fc03747675ab3aa179
SamanthaPiresLuchmannLeal <samantha.luchmann@mercadolivre.com>
2022-07-10 12:04:06 -0300

    Exemplo de extends de interface

---
+++ figuras/src/interfaces/ExibirPlus.java
@@ -0,0 +1,8 @@
+package interfaces;
+
+public interface ExibirPlus extends Exibir{
+    void exibirColorido();
+//    é possivel extender uma interface de outra interface, e obrigatoriamente
+//    os métodos serão virao de ambas as interfaces
+
+}
