776d8d8e8921a24e09bf7e4f5ae721df5f794e34
SamanthaPiresLuchmannLeal <samantha.luchmann@mercadolivre.com>
2022-07-10 13:33:49 -0300

    Coleções List e Map

--- exemploInterface/src/AppCont.java
+++ exemploInterface/src/AppCont.java
@@ -1,7 +1,12 @@
+import modelo.Conta;
 import modelo.ContaCorrente;
 import modelo.ContaEspecial;
 import util.MongoDB;
 import util.MySQL;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class AppCont {
     public static void main(String[] args) {
@@ -11,9 +16,30 @@ public class AppCont {
         MySQL mySQL = new MySQL();
         MongoDB mongoDB = new MongoDB();
 
-        cc1.gravar((mySQL));
+        ArrayList<Conta> listaDeContas = new ArrayList();
+        HashMap<Integer, Conta> mapaDeContas = new HashMap<>();
 
-        ce1.gravar(mongoDB);
+        listaDeContas.add(cc1);
+        listaDeContas.add(ce1);
+
+//        for (int i = 0; i < contas.size(); i++){
+//            System.out.println("Conta: " + contas.get(i).getNumero());
+//        }
+
+
+
+//        cc1.gravar((mySQL));
+//        ce1.gravar(mongoDB);
+
+        mapaDeContas.put(cc1.getNumero(),cc1);
+        mapaDeContas.put(ce1.getNumero(),ce1);
+
+        for (Conta c : listaDeContas ) { //para cada Conta c da estrutura  listaDeContas
+            System.out.println("Conta no array: " + c.getNumero());
+        }
+
+        Conta contaEncontrada = mapaDeContas.get(111);
+        System.out.println("Conta encontrada no HashMAp: " + contaEncontrada.getNumero());
 
     }
 }
