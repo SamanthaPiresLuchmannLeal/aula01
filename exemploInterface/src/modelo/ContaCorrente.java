89f5154423e87e5ca56dd9bcc647c71fd72dd184
SamanthaPiresLuchmannLeal <samantha.luchmann@mercadolivre.com>
2022-07-10 12:51:51 -0300

    Exemplo interface

---
+++ exemploInterface/src/modelo/ContaCorrente.java
@@ -0,0 +1,14 @@
+package modelo;
+
+public class ContaCorrente extends Conta{
+    public ContaCorrente(int numero) {
+        super(numero);
+    }
+
+    @Override
+    public void sacar(double valor) {
+        if(valor <= getSaldo()) {
+            super.sacar(valor);
+        }
+    }
+}
