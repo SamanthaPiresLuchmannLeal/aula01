7ef421bd08d1eda8c786b64af49fdfe6151651f1
SamanthaPiresLuchmannLeal <samantha.luchmann@mercadolivre.com>
2022-07-26 10:52:56 -0300

    Usando elasticsearch

---
+++ spring_elastic/src/main/java/dh/meli/spring_elastic/controller/ArticleController.java
@@ -0,0 +1,25 @@
+package dh.meli.spring_elastic.controller;
+
+import dh.meli.spring_elastic.model.Article;
+import dh.meli.spring_elastic.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/article")
+public class ArticleController {
+
+    @Autowired
+    public ArticleService service;
+
+    @PostMapping
+    public ResponseEntity<Article> save (@RequestBody Article article) {
+        return ResponseEntity.ok(service.save(article));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Article> findById(@PathVariable int id) {
+        return ResponseEntity.ok(service.findById(id));
+    }
+}
---
+++ spring_elastic/src/main/java/dh/meli/spring_elastic/service/IArticleService.java
@@ -0,0 +1,9 @@
+package dh.meli.spring_elastic.service;
+
+import dh.meli.spring_elastic.model.Article;
+
+public interface IArticleService {
+    Article save(Article article);
+    Article findById(int id);
+
+}
